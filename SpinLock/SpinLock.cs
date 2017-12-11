c4d2d104c4ba5dee3c97ec27a4641c1d7e6a7cb1
SenpaiKush <matt31996@gmail.com>
2017-12-11 23:20:46 +0100

    Add project files.

---
+++ SpinLock/SpinLock.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace SpinLock
+{
+    public abstract class SpinLock
+    {
+        private int TokenCounter;
+        private volatile int ServedToken;
+
+        public SpinLock()
+        {
+            ServedToken = TokenCounter + 1;
+        }
+
+        protected void SpinWaitFor(Action action)
+        {
+            int ThreadToken = Interlocked.Increment(ref TokenCounter);
+
+            while (true)
+            {
+                if (Interlocked.CompareExchange(ref ThreadToken, ServedToken + 1, ServedToken) != ThreadToken)
+                {
+                    action.Invoke();
+                    ServedToken = ThreadToken;
+                    break;
+                }
+            }
+        }
+    }
+}
